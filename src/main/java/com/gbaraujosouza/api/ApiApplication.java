7760e3a6399334711cf797b7151d36960d9273c9
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-11-03 14:56:32 -0300

    init commit

---
+++ src/main/java/com/gbaraujosouza/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.gbaraujosouza.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbaraujosouza/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbaraujosouza.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
